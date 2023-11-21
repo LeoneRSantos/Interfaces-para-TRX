cab7c495b7053183ae40d2d4a968c80b0f7e5e63
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-11-21 16:46:09 -0300

    Redirecionamento para a tela de transmissão em index.js

--- index.js
+++ index.js
@@ -11,13 +11,16 @@ app.get("/home", function (req, res) {
     res.sendFile(__dirname + "/home.html")
 })
 
+app.get("/transmission", function (req, res) {
+    res.sendFile(__dirname + "/trasmission.html")
+})
 
 app.get("/run-log/:parametro", (req, res) => {
     const parametro = req.params.parametro;
     log(parametro);
     res.send(`Log executado com sucesso para o parâmetro: ${parametro}`);
-  });
+});
 
-  
+
 
 app.listen(3000);
